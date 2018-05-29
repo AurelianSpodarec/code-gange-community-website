fa85adcc9ae0957a08b481d03cfd449619c4d811
Colton Ehrman <coltonehrman@Coltons-MacBook-Pro.local>
2018-05-29 18:33:29 -0500

    Initial commit

---
+++ dist/server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+app.use(_express2.default.static('src'));
+
+app.listen(3000, function () {
+    console.log('Server is listening on http://localhost:3000');
+});
+
+//# sourceMappingURL=server.js.map

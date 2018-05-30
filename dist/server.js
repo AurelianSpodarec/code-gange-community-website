7998d3b208c3306ab0ca47d9d84802e58489111e
Colton Ehrman <coltonehrman@Coltons-MacBook-Pro.local>
2018-05-29 19:11:04 -0500

    Simple starter bootstrap landing page template

--- dist/server.js
+++ dist/server.js
@@ -7,10 +7,9 @@ var _express2 = _interopRequireDefault(_express);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
+var PORT = process.env.PORT || 3000;
 
 app.use(_express2.default.static('src'));
-
-var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function () {
     console.log('Server is listening on http://localhost:' + PORT);
--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 import express from 'express';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('src'));
-
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
